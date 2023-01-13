0c914a66550d6afd834c6fe2ec2dd7908b69d690
Alexander Babak <bbkshow1987@gmail.com>
2023-01-13 11:16:17 +0200

    check new feature of git

--- src/screens/PaymentScreen.tsx
+++ src/screens/PaymentScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Center, Heading } from 'native-base';
+import { Center, Heading, Text } from 'native-base';
 
 export const PaymentScreen = () => {
   return (
     <Center flex={1}>
       <Heading>PaymentScreen</Heading>
+      <Text>Check this screen for commit</Text>
     </Center>
   );
 };
