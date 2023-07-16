9727fe867666a9ca5d3d07df957f4e04ca0f0ac4
AlexandrBabac <139109354+AlexandrBabac@users.noreply.github.com>
2023-07-16 14:38:17 +0300

    add testfile

---
+++ testfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export const TestFile = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
